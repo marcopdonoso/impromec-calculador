852be8f30daacb97f72c7923c48b06929989b589
Marco Perez Donoso <marcopdonoso@gmail.com>
2024-06-10 14:26:33 -0400

    agrega componente Typography y icon de la app

---
+++ src/components/Typography.tsx
@@ -0,0 +1,96 @@
+import clsx from 'clsx'
+import { ElementType, ReactNode } from 'react'
+
+type TypographyVariant =
+	| 'heading_h4'
+	| 'heading_h6'
+	| 'body_large_semibold'
+	| 'body_large_regular'
+	| 'body_medium_bold'
+	| 'body_medium_medium'
+	| 'body_medium_regular'
+	| 'body_small_medium'
+	| 'body_small_regular'
+	| 'body_xs_medium'
+	| 'body_xs_regular'
+	| 'body_xxs_regular'
+
+interface VariantProps {
+	tag: ElementType
+	className: string
+}
+
+const variants: Record<TypographyVariant, VariantProps> = {
+	heading_h4: {
+		tag: 'h4',
+		className: 'text-3xl font-bold',
+	},
+	heading_h6: {
+		tag: 'h6',
+		className: 'text-2xl font-semibold',
+	},
+	body_large_semibold: {
+		tag: 'p',
+		className: 'text-lg font-semibold',
+	},
+	body_large_regular: {
+		tag: 'p',
+		className: 'text-lg font-normal',
+	},
+	body_medium_bold: {
+		tag: 'p',
+		className: 'text-base font-bold',
+	},
+	body_medium_medium: {
+		tag: 'p',
+		className: 'text-base font-semibold',
+	},
+	body_medium_regular: {
+		tag: 'p',
+		className: 'text-base font-normal',
+	},
+	body_small_medium: {
+		tag: 'p',
+		className: 'text-sm font-semibold',
+	},
+	body_small_regular: {
+		tag: 'p',
+		className: 'text-sm font-normal',
+	},
+	body_xs_medium: {
+		tag: 'p',
+		className: 'text-xs font-semibold',
+	},
+	body_xs_regular: {
+		tag: 'p',
+		className: 'text-xs font-normal',
+	},
+	body_xxs_regular: {
+		tag: 'p',
+		className: 'text-[10px] font-normal',
+	},
+}
+
+interface TypographyProps {
+	variant?: TypographyVariant
+	className?: string
+	children: ReactNode
+}
+
+export default function Typography({
+	variant = 'body_medium_regular',
+	className,
+	children,
+	...props
+}: TypographyProps) {
+	const Component = variants[variant].tag
+
+	return (
+		<Component
+			className={clsx(variants[variant].className, className)}
+			{...props}
+		>
+			{children}
+		</Component>
+	)
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,40 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		colors: {
+			red: '#DA0027',
+			red_alt: '#FEEBEB',
+			yellow: '#FABC00',
+			green: {
+				success: '#22AD5C',
+				success_alt: '#DAF8E6',
+			},
+			gray: {
+				button_primary: '#3F3F3F',
+				text: '#1F2A37',
+				text_alt: '#4B5563',
+				placeholder_icon: '#6B7280',
+				text_inactive: '#637381',
+				placeholder: '#9CA3AF',
+				input: '#DFE4EA',
+				background: '#F2F2F2',
+				background_alt: '#FAFAFA',
+			},
+		},
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+		},
+	},
+	plugins: [],
+}
+export default config
