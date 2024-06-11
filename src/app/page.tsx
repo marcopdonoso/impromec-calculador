1fed24cba6affe303149d80e33bf97fc6feba188
Marco Perez Donoso <marcopdonoso@gmail.com>
2024-06-11 11:35:10 -0400

    agrega componente Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
-import Typography from '@/components/Typography'
+import Button from '@/components/Button'
 
 export default function Home() {
-	return <Typography variant="heading_h4">Hello, world!</Typography>
+  const Icon = () => (
+    <div className="flex h-4 w-4 items-center justify-center">+</div>
+  )
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Button variant="primary" icon={<Icon />}>
+        Prueba
+      </Button>
+    </div>
+  )
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,105 @@
+import clsx from 'clsx'
+type ButtonVariants =
+  | 'primary'
+  | 'primary_small'
+  | 'secondary'
+  | 'secondary_small'
+  | 'tertiary'
+  | 'tertiary_small'
+  | 'destructive'
+  | 'destructive_small'
+  | 'icon_right'
+  | 'icon_right_small'
+  | 'add'
+  | 'add_small'
+  | 'register'
+  | 'login'
+
+interface VariantProps {
+  className: string
+}
+
+const variants: Record<ButtonVariants, VariantProps> = {
+  primary: {
+    className:
+      'h-12 w-80 rounded-lg bg-gray-button_primary text-sm font-medium text-gray-white hover:bg-gray-placeholder_icon active:bg-gray-button_primary disabled:bg-gray-input disabled:text-gray-text_inactive sm:w-1/3 sm:text-base',
+  },
+  primary_small: {
+    className:
+      'text-gray-white h-12 w-52 rounded-lg bg-gray-button_primary text-sm font-medium hover:bg-gray-placeholder_icon active:bg-gray-button_primary disabled:bg-gray-input disabled:text-gray-text_inactive sm:text-base',
+  },
+  secondary: {
+    className:
+      'bg-gray-white active:bg-gray-white h-12 w-80 rounded-lg border border-gray-input text-sm font-medium text-gray-text hover:bg-gray-background sm:w-1/3 sm:text-base',
+  },
+  secondary_small: {
+    className:
+      'bg-gray-white active:bg-gray-white h-12 w-52 rounded-lg border border-gray-input text-sm font-medium text-gray-text hover:bg-gray-background sm:text-base',
+  },
+  tertiary: {
+    className:
+      'h-12 w-80 rounded-lg border border-gray-input bg-gray-background text-sm font-medium text-gray-text hover:bg-gray-input active:bg-gray-background sm:w-1/3 sm:text-base',
+  },
+  tertiary_small: {
+    className:
+      'h-12 w-52 rounded-lg border border-gray-input bg-gray-background text-sm font-medium text-gray-text hover:bg-gray-input active:bg-gray-background sm:text-base',
+  },
+  destructive: {
+    className:
+      'text-gray-white active:text-gray-white h-12 w-80 rounded-lg bg-red text-sm font-medium hover:bg-red_alt hover:text-red active:bg-red sm:w-1/3 sm:text-base',
+  },
+  destructive_small: {
+    className:
+      'text-gray-white active:text-gray-white h-12 w-52 rounded-lg bg-red text-sm font-medium hover:bg-red_alt hover:text-red active:bg-red sm:text-base',
+  },
+  icon_right: {
+    className:
+      'text-gray-white rounded-lg bg-gray-button_primary p-3 text-sm font-medium hover:bg-gray-placeholder_icon active:bg-gray-button_primary sm:h-12 sm:px-4 sm:py-0 sm:text-base',
+  },
+  icon_right_small: {
+    className:
+      'text-gray-white h-12 w-52 rounded-lg bg-gray-button_primary px-5 text-sm font-medium hover:bg-gray-placeholder_icon active:bg-gray-button_primary sm:text-base',
+  },
+  add: {
+    className:
+      'bg-gray-white active:bg-gray-white h-12 w-80 rounded-lg border border-green-success text-sm font-medium text-green-success hover:bg-green-success_alt sm:w-2/3 sm:text-base',
+  },
+  add_small: {
+    className:
+      'bg-gray-white active:bg-gray-white h-12 w-52 rounded-lg border border-green-success text-sm font-medium text-green-success hover:bg-green-success_alt sm:text-base',
+  },
+  register: {
+    className:
+      'h-10 w-52 rounded-lg text-sm font-medium text-gray-text active:border active:border-gray-input active:bg-gray-background sm:w-36 sm:text-base',
+  },
+  login: {
+    className:
+      'text-gray-white h-10 w-52 rounded-lg bg-gray-button_primary text-sm font-medium hover:bg-gray-placeholder_icon active:bg-gray-button_primary sm:w-36 sm:text-base',
+  },
+}
+
+interface ButtonProps {
+  variant?: ButtonVariants
+  children?: React.ReactNode
+  icon?: React.ReactNode
+  className?: string
+}
+
+export default function Button({
+  variant = 'primary',
+  children,
+  icon,
+  className,
+  ...props
+}: ButtonProps) {
+  return (
+    <button className={clsx(variants[variant].className, className)} {...props}>
+      <div
+        className={`flex items-center ${variant === 'icon_right' || variant === 'icon_right_small' ? 'flex-row-reverse justify-between gap-4' : 'justify-center gap-2'}`}
+      >
+        {icon}
+        {children}
+      </div>
+    </button>
+  )
+}
