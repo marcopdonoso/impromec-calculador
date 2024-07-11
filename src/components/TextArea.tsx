906bec7b3c32e7f7520607bff14ea73cf4695dc3
Marco Perez Donoso <marcopdonoso@gmail.com>
2024-07-11 17:27:26 -0400

    cambia responsividad a Textarea

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -13,13 +13,13 @@ export default function Textarea({
   return (
     <label
       className={
-        'flex h-40 w-80 flex-col justify-start gap-1 text-sm font-medium sm:w-[32vw] sm:gap-2 sm:text-base'
+        'flex h-40 w-80 flex-col justify-start gap-1 text-sm font-medium lg:w-[32vw] lg:gap-2 lg:text-base'
       }
     >
       {label}
       <textarea
         className={clsx(
-          'resize-none rounded-lg border border-gray-input bg-gray-white p-5 text-sm font-normal text-gray-text placeholder:text-sm placeholder:text-gray-placeholder focus:border-gray-placeholder focus:outline-none focus:ring-0 disabled:text-gray-text_inactive sm:text-base sm:placeholder:text-base',
+          'resize-none rounded-lg border border-gray-input bg-gray-white p-5 text-sm font-normal text-gray-text placeholder:text-sm placeholder:text-gray-placeholder focus:border-gray-placeholder focus:outline-none focus:ring-0 disabled:text-gray-text_inactive lg:text-base lg:placeholder:text-base',
           className
         )}
         {...props}
