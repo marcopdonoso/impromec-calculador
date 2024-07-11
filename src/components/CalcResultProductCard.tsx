34a58639a4ec8a9eefa1b8a9ecc7242589485f06
Marco Perez Donoso <marcopdonoso@gmail.com>
2024-07-11 16:53:27 -0400

    cambia responsividad a CalcResultProductCard

--- src/components/CalcResultProductCard.tsx
+++ src/components/CalcResultProductCard.tsx
@@ -23,16 +23,16 @@ export default function CalcResultProductCard({
   firstOption,
 }: CalcResultProductCardProps) {
   return (
-    <div className="w-80 rounded-lg bg-gray-input px-4 py-5 sm:w-[66vw] sm:p-6">
-      <div className="sm:flex sm:items-center sm:justify-between">
-        <p className="text-sm font-medium sm:text-2xl sm:font-semibold">
+    <div className="w-80 rounded-lg bg-gray-input px-4 py-5 lg:w-[66vw] lg:p-6">
+      <div className="lg:flex lg:items-center lg:justify-between">
+        <p className="text-sm font-medium lg:text-2xl lg:font-semibold">
           {title}
         </p>
-        <p className="text-sm sm:text-lg">{subtitle}</p>
+        <p className="text-sm lg:text-lg">{subtitle}</p>
       </div>
 
-      <div className="relative mt-4 flex flex-wrap gap-2 sm:flex-nowrap sm:gap-6">
-        <div className="relative size-16 min-w-16 rounded-lg bg-gray-background sm:size-36 sm:min-w-36">
+      <div className="relative mt-4 flex flex-wrap gap-2 lg:flex-nowrap lg:gap-6">
+        <div className="relative size-16 min-w-16 rounded-lg bg-gray-background lg:size-36 lg:min-w-36">
           <Image
             fill
             src={image ?? '/svg/landscape-placeholder.svg'}
@@ -43,10 +43,10 @@ export default function CalcResultProductCard({
         </div>
 
         <div>
-          <p className="absolute right-0 top-0 max-w-52 text-sm font-medium sm:static sm:mt-2 sm:max-w-full sm:text-base">
+          <p className="absolute right-0 top-0 max-w-52 text-sm font-medium lg:static lg:mt-2 lg:max-w-full lg:text-base">
             {description}
           </p>
-          <div className="mt-4 flex gap-4 sm:mt-1 sm:flex-col sm:gap-1">
+          <div className="mt-4 flex gap-4 lg:mt-1 lg:flex-col lg:gap-1">
             <ListItem style={{ color: '#1F2A37' }}>
               Alto (h): {height} mm
             </ListItem>
@@ -55,7 +55,7 @@ export default function CalcResultProductCard({
             </ListItem>
           </div>
           {firstOption && (
-            <p className="mt-2 hidden sm:block">
+            <p className="mt-2 hidden lg:block">
               Materia prima certificada y fabricada bajo normativa{' '}
               <span className="font-medium">ASTM A653</span> de grado
               estructural. Calidad garantizada bajo norma{' '}
