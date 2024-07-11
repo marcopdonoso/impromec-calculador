a62c340ed226ebcf803ac1ea20a554d482fc0ac9
Marco Perez Donoso <marcopdonoso@gmail.com>
2024-07-11 16:59:24 -0400

    cambia responsividad a Input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -23,12 +23,12 @@ export default function Input({
     <label
       className={clsx(
         className,
-        'relative flex flex-col justify-start gap-1 text-sm font-medium sm:gap-2 sm:text-base',
+        'relative flex flex-col justify-start gap-1 text-sm font-medium lg:gap-2 lg:text-base',
         {
-          'w-40 sm:w-[14vw]': variant === 'small',
-          'w-80 sm:w-[19vw]': variant === 'third',
-          'w-80 sm:w-[32vw]': variant === 'default',
-          'w-80 sm:w-[66vw]': variant === 'large',
+          'w-40 lg:w-[14vw]': variant === 'small',
+          'w-80 lg:w-[19vw]': variant === 'third',
+          'w-80 lg:w-[32vw]': variant === 'default',
+          'w-80 lg:w-[66vw]': variant === 'large',
         }
       )}
       htmlFor={props.name}
@@ -39,7 +39,7 @@ export default function Input({
         min={0}
         id={props.name}
         className={clsx(
-          'h-12 rounded-lg border border-gray-input bg-gray-white pl-5 pr-4 text-sm font-normal text-gray-text placeholder:text-sm placeholder:text-gray-placeholder focus:border-gray-placeholder focus:outline-none focus:ring-0 disabled:text-gray-text_inactive sm:text-base sm:placeholder:text-base'
+          'h-12 rounded-lg border border-gray-input bg-gray-white pl-5 pr-4 text-sm font-normal text-gray-text placeholder:text-sm placeholder:text-gray-placeholder focus:border-gray-placeholder focus:outline-none focus:ring-0 disabled:text-gray-text_inactive lg:text-base lg:placeholder:text-base'
         )}
         {...props}
       />
