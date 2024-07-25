7d463f25e005cb11b57dc99501aafa3ef495cc50
Marco Perez Donoso <marcopdonoso@gmail.com>
2024-07-25 13:11:46 -0400

    landing-page agrega ProductsSection responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import CalculatorSection from '@/components/landing/CalculatorSection'
+import ProductsSection from '@/components/landing/ProductsSection'
 import TopSection from '@/components/landing/TopSection'
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <section className="flex flex-col items-center pt-16 lg:pt-20">
       <TopSection />
       <CalculatorSection />
+      <ProductsSection />
     </section>
   )
 }
