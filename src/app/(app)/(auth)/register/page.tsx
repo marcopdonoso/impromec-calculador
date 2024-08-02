20477adf18997173228720cb1be885b4acfd36b2
Marco Perez Donoso <marcopdonoso@gmail.com>
2024-08-02 10:36:36 -0400

    register-page crea componente RegisterPage

---
+++ src/app/(app)/(auth)/register/page.tsx
@@ -0,0 +1,15 @@
+export default function RegisterPage() {
+  return (
+    <main className="px-2 py-8">
+      <section className="rounded-lg bg-gray-background">
+        <h4 className="body_large_semibold text-center">
+          ¡Regístrate y comienza hoy!
+        </h4>
+        <p className="body_small_regular text-center">
+          Accede y utiliza nuestra calculadora de bandejas portacables de forma
+          segura y confiable.
+        </p>
+      </section>
+    </main>
+  )
+}
