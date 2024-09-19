247f2cbc7358a7ef2055992aa775d0f4051757c1
Marco Perez Donoso <marcopdonoso@gmail.com>
2024-09-19 13:02:05 -0400

    calculador: refactor

--- src/app/(app)/auth/reset-password/page.tsx
+++ src/app/(app)/auth/reset-password/page.tsx
@@ -1,8 +1,6 @@
-import Button from '@/components/Button'
-import InputPass from '@/components/InputPass'
 import { authLinks } from '@/constants/links.constants'
-import Link from 'next/link'
 import AuthFormCard from '../components/AuthFormCard'
+import ResetPasswordForm from './components/ResetPasswordForm'
 
 export default function ResetPasswordPage() {
   return (
@@ -15,26 +13,7 @@ export default function ResetPasswordPage() {
         footerLinkText="Registrate"
         fullwidth={false}
       >
-        <form className="mt-6 flex w-full flex-col items-center lg:mt-10">
-          <InputPass
-            label="Nueva contraseña"
-            placeholder="8+caracteres: letras, números, símbolos"
-            className="mb-4 lg:mb-6"
-          />
-          <InputPass
-            label="Confirmar contraseña"
-            placeholder="8+caracteres: letras, números, símbolos"
-            className="mb-14 lg:mb-20"
-          />
-          <Button type="submit" className="mb-4">
-            Cambiar contraseña
-          </Button>
-          <Link href={authLinks.login.path} className="w-full rounded-lg">
-            <Button type="button" variant="secondary">
-              Cancelar
-            </Button>
-          </Link>
-        </form>
+        <ResetPasswordForm />
       </AuthFormCard>
     </main>
   )
---
+++ src/app/(app)/dashboard/calculator/new-project/components/NewProjectForm.tsx
@@ -0,0 +1,29 @@
+import Button from '@/components/Button'
+import Input from '@/components/Input'
+import { appLinks } from '@/constants/links.constants'
+import Link from 'next/link'
+import SectorSwitch from './SectorSwitch'
+
+export default function NewProjectForm() {
+  return (
+    <form className="mt-12 w-full lg:mt-14 lg:max-w-4xl">
+      <Input
+        label="Nombre del proyecto"
+        placeholder="Ej: Instalación planta de fabricación"
+      />
+      <div className="mt-4 flex flex-col gap-4 lg:mt-6 lg:flex-row lg:gap-8">
+        <Input label="Empresa" placeholder="Ej: Empresa ABC" />
+        <Input label="Locación" placeholder="Ej: Cochabamba, Bolivia" />
+      </div>
+      <SectorSwitch />
+      <div className="mt-8 flex w-full flex-col gap-4 lg:flex-row-reverse lg:gap-9">
+        <Button type="submit">Continuar</Button>
+        <Link href={appLinks.calculatorHome.path} className="lg:w-full">
+          <Button variant="secondary" type="button">
+            Volver atrás
+          </Button>
+        </Link>
+      </div>
+    </form>
+  )
+}
