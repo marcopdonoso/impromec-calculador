6f8c5a0e882852400154c72b5d11a3249702401f
Marco Perez Donoso <marcopdonoso@gmail.com>
2024-10-01 16:57:13 -0400

    calculador: fix typos en vista de add-sector

--- src/app/(app)/dashboard/calculator/edit-project/add-sector/page.tsx
+++ src/app/(app)/dashboard/calculator/edit-project/add-sector/page.tsx
@@ -6,10 +6,12 @@ import { useRouter } from 'next/navigation'
 
 export default function AddSectorPage() {
   const router = useRouter()
+
   const handleClick = () => {
-    console.log('borrar proyecto actual') //TODO: cambiar por servicio de petición al backend
+    console.log('borrar proyecto actual') //TODO: cambiar por llamada al servicio correspondiente
     router.push('/dashboard/calculator/new-project')
   }
+
   return (
     <section className="flex min-h-screen w-full max-w-4xl flex-col px-4 pt-8">
       <Input
@@ -24,12 +26,12 @@ export default function AddSectorPage() {
       <div className="mt-10">
         <Message title="Recuerda que seleccionaste dividir el proyecto en sectores.">
           Cuando divides el proyecto en sectores debes agregar al menos un
-          sector y así podrás comenzar con el calculo{' '}
+          sector y así podrás comenzar con el cálculo,{' '}
           <span
             onClick={handleClick}
-            className="body_small_medium cursor-pointer text-green-success underline"
+            className="body_small_medium cursor-pointer text-green-success underline lg:body_medium_medium"
           >
-            o puedes crear el proyecto desde 0.
+            o puedes crear el proyecto desde cero.
           </span>
         </Message>
       </div>
