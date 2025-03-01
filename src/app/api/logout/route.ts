5ca856556e8af9611deb53b917410341e5a241bc
Marco Perez Donoso <marcopdonoso@gmail.com>
2025-03-01 01:40:37 -0400

    fix corrige estructura del handler de api/logout

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,8 +1,16 @@
-import { deleteCookie } from 'cookies-next'
+import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
-export async function GET(req: any, res: any) {
-  deleteCookie('token', { req, res, path: '/', sameSite: 'strict' })
+export async function GET() {
+  cookies().set({
+    name: 'token',
+    value: '',
+    expires: new Date(0),
+    path: '/',
+    sameSite: 'strict',
+    secure: process.env.NODE_ENV === 'production',
+    httpOnly: true,
+  })
 
   return NextResponse.json({ message: 'Logged out' }, { status: 200 })
 }
