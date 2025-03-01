26e20d58a783d6b7ebefb9b56694916f236f253a
Marco Perez Donoso <marcopdonoso@gmail.com>
2025-03-01 01:06:59 -0400

    agrega parametros a la configuracion de la cookie token en ruta de logout para coincidir con la configuracion de la creacion en login

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -3,6 +3,13 @@ import { NextResponse } from 'next/server'
 export async function GET() {
   const response = NextResponse.json({ message: 'Logged out' }, { status: 200 })
 
-  response.cookies.set('token', '', { expires: new Date(0), path: '/' })
+  response.cookies.set('token', '', {
+    expires: new Date(0),
+    path: '/',
+    sameSite: 'strict',
+    secure: process.env.NODE_ENV === 'production',
+    httpOnly: true,
+  })
+
   return response
 }
